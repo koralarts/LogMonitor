b330886f332360ae6f9ea2e81ad11b18e7ee754a
Karl Castillo <khcastillo@hotmail.com>
2012-02-29 11:49:42 -0800

    fixed using awk

--- monitor.sh
+++ monitor.sh
@@ -4,17 +4,18 @@
 # Authors:
 #	Karl Castillo
 #	Daniel Khatkar
-##############################################
+#####################################
+#########
 
 ##############################################
 # USER VARIABLES
 ##############################################
-BLOCK_TRIES="3"
-BLOCK_DELAY="10s"
-BLOCK_TIL="1d"
-OS="ubuntu"
-SERVICE="ssh"
-LOG_LOC="/var/log/auth.log"
+BLOCK_TRIES="3"			# How many tries before blocking
+BLOCK_DELAY="10"		# The delay in seconds
+BLOCK_TIL="1d"			# The amount of time to block the IP
+OS="ubuntu"			# The current OS
+SERVICE="ssh"			# Service to be checked
+LOG_LOC="/var/log/auth.log"	# The location of the log file
 
 ##############################################
 # DO NOT TOUCH
@@ -22,49 +23,54 @@ LOG_LOC="/var/log/auth.log"
 
 # SERVICE LOG REGEXP
 SSH="sshd\[[0-9]+\]: Failed password"
-
+	
 #NORMAL VARIABLES
-COUNTER=0
-TRY_COUNTER=0
+TRIES=0
+IFS=$'\n'
 
+#############################################
+# HELPER FUNCTIONS
+#############################################
+function sshMonitor()
+{	
+	for LINE in `grep -E "$PATTERN" $LOG_LOC`
+	do
+		eval `echo $LINE | awk '
+				BEGIN {
+					IP_ADDRESS=""
+					TIMESTAMP=""
+				} 
+				{ IP_ADDRESS=$11 }
+				{ TIMESTAMP=$3 }
+				END {
+					printf "IP_ADDRESS=\"%s\"\n", IP_ADDRESS
+					printf "TIMESTAMP=\"%s\"\n", TIMESTAMP
+				}
+				'`
+		echo "============================"
+		echo "IP_ADDRESS = $IP_ADDRESS"
+		echo "TIMESTAMP = $TIMESTAMP"
+		echo "============================"
+	done
+}
+
+# Check if file exists
+if [ ! -f $LOG_LOC ]
+then
+	echo "$LOG_LOC: does not exist"
+	return 1
+fi
+
+# Check if file can be read
+if [ ! -r $LOG_LOG]
+then
+	echo "$LOGLOC: cannot read file"
+	return 2
+fi
+
+# Determine type of service
 if [[ $SERVICE="ssh" ]]
 then
 	PATTERN=$SSH
 	sshMonitor
 fi
-
-function sshMonitor()
-{
-	for line in `grep -E "$PATTERN" $LOG_LOC`
-	do
-		echo 'line ='${line}
-		echo 'counter = '${COUNTER}
-
-		case ${COUNTER} in
-			2)	if [[ "$BLOCK_DELAY" =~ ^[0-9]+s$ ]]
-				then
-					echo "seconds"
-				fi
-
-				if [[ "$BLOCK_DELAY" =~ ^[0-9]+m$ ]]
-				then
-					echo "minutes"
-				fi
-
-				if [[ "$BLOCK_DELAY" =~ ^[0-9]+h$ ]]
-				then
-					echo "hours"
-				fi
-
-				COUNTER=`expr $COUNTER + 1`
-			;;
-			10) 	IP_ADDRESS=$line
-				COUNTER=`expr $COUNTER + 1`
-			;;
-			13) 	IP_ADDRESS=""
-				COUNTER=0
-			;;
-			*)	COUNTER=`expr $COUNTER + 1`
-		esac
-	done
-}
