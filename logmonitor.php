1324f59eb73ed9b452f610f8a75391ac08bb3429
Karl Castillo <khcastillo@hotmail.com>
2012-03-04 18:56:06 -0800

    fixed and added error checking

--- logmonitor.php
+++ logmonitor.php
@@ -8,7 +8,7 @@
 		 * User-Defined Variables
 		 ****************************************/
 		$BLOCK_TRIES=3;			// Number of tries before blocking
-		$BLOCK_TIL="1 minute";		// Time limit for blocking
+		$BLOCK_TIL="5 minutes";		// Time limit for blocking
 		$SERVICE="ssh";			// The service to be checked
 		$PROTOCOL="tcp";		// The protocol of the service
 		$LOG_LOC="/var/log/auth.log";	// The location of the log
@@ -35,26 +35,36 @@
 		// Grab everything failed attempts from the log file
 		exec("tac $LOG_LOC | grep -m 1000 -E \"$pattern\"", &$output);
 
-		foreach($output as $value) {
-			$line = explode(" ", $value);
+		for($i = 0; $i < count($output); $i++) {
+			$line = explode(" ", $output[$i]);
 			if(!isset($ips[$line[$ip]])) {
-				$ips[$line[$ip]] = 0;
+				$ips[$line[$ip]] = 1;
 			} else {
 				$ips[$line[$ip]]++;
 			}
 
 			if($ips[$line[$ip]] == $BLOCK_TRIES) {
-				blockIP($line[$ip], $PROTOCOL, $SERVICE);
-				unBlockIP($line[$ip], $PROTOCOL, $SERVICE, $BLOCK_TIL);
+				$found = "";
+				exec("iptables -L INPUT | grep -c \"$line[$ip]\"", &$found);
+				if($found[0] == 0) { // Check if IP is already blocked				
+					blockIP($line[$ip], $PROTOCOL, $SERVICE);
+					unBlockIP($line[$ip], $PROTOCOL, $SERVICE, $BLOCK_TIL);
+				}
 			}
 		}
 	}
 
+	/****************************************
+	 * Function to Block the IP
+	 ****************************************/
 	function blockIP($ip, $protocol, $service)
 	{
 		exec("iptables -A INPUT -s \"$ip\" -p \"$protocol\" --dport \"$service\" -j DROP");
 	}
 
+	/****************************************
+	 * Function to set the delay before unblocking the IP
+	 ****************************************/
 	function unBlockIP($ip, $protocol, $service, $delay)
 	{
 		$date = "";
